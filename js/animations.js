b8e85ba655544eab4e13d9f4f96e452ec4fa8081
sNishant011 <49269819+sNishant011@users.noreply.github.com>
2021-01-06 20:57:13 +0545

    animation

--- js/animations.js
+++ js/animations.js
@@ -3,30 +3,17 @@ gsap.to(".skills", {
     scrollTrigger: {
         trigger: ".container .img",
         start: "top top",
-        markers: true,
-        scrub: true,
         toggleActions: "restart play reverse reset"
     },
     backgroundColor: "#fff",
+    duration:1,
 });
 gsap.from(".showcase", {
     scrollTrigger: {
         trigger: ".skills",
         start: "bottom center",
-        markers: true,
-        scrub: true,
         toggleActions: "restart play reverse reset"
     },
     backgroundColor: "#fff",
-});
-gsap.to(".showcase h1", {
-    scrollTrigger: {
-        trigger: ".skills",
-        start: "bottom center",
-        markers: true,
-        scrub: true,
-        toggleActions: "restart play reverse reset"
-    },
-    color: "#fff",
-    textDecoration: "none",
+    druation:1,
 });
