47dc9fa82cbe8df946266bbc4b5d07a176d39a2d
sNishant011 <49269819+sNishant011@users.noreply.github.com>
2021-01-09 18:41:16 +0545

    companies scroll animation

--- js/animations.js
+++ js/animations.js
@@ -12,8 +12,9 @@ gsap.from(".showcase", {
     scrollTrigger: {
         trigger: ".showcase",
         start: "top center",
-        toggleActions: "restart play reverse reset"
+        toggleActions: "restart play none reset"
     },
+    color: "#000",
     backgroundColor: "#fff",
     druation:1,
 });
@@ -21,7 +22,7 @@ gsap.from(".myskills .card", {
     scrollTrigger:{
         trigger: ".skills",
         start: "top bottom",
-        toggleActions: "restart pause none none",
+        toggleActions: "restart none none none",
     },
     opacity:0,
     y:300,
@@ -34,7 +35,7 @@ gsap.from(".projects .card", {
     scrollTrigger:{
         trigger: ".projects",
         start: "top bottom",
-        toggleActions: "restart pause none none",
+        toggleActions: "restart none none none",
 
     },
     opacity:0,
@@ -43,3 +44,49 @@ gsap.from(".projects .card", {
     stagger:0.25,
 
 });
+
+var gallerytl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".companies",
+        scrub: true,
+        pin: true,
+        start: "top top",
+    }
+});
+
+gallerytl.from("#envisionerl", {
+    scale: 0.3,
+    autoAlpha: 0,
+    ease: "slow",
+    duration: 10,
+})
+var insertpos = gallerytl.duration();
+gallerytl.from("#snphotosl", {
+    scale: 0.3,
+    autoAlpha: 0,
+    ease: "slow",
+    duration: 10,
+    visibility: "visible",
+    x: "-15vw",
+    y: "-10vh",
+}, (insertpos -= 1.5))
+insertpos += 0.4;
+gallerytl.from("#nepcodel", {
+    scale: 0.3,
+    autoAlpha: 0,
+    ease: "slow",
+    duration: 10,
+    visibility: "visible",
+    x: "80vw",
+    y: "-10vh",
+}, (insertpos -= 0.2))
+insertpos += 0.4;
+gallerytl.from("#ebakeryl", {
+    scale: 0.3,
+    autoAlpha: 0,
+    ease: "slow",
+    duration: 10,
+    visibility: "visible",
+    y: "60vh",
+}, (insertpos -= 0.5))
+insertpos += 0.4;
